050b8e6247ae8d2fb7a50f943aa7d885fa9e60f7
eureka1123 <xl.elaine.guo@gmail.com>
2017-08-17 21:15:18 +0000

    fixed bug special characters

--- create_training_data.py
+++ create_training_data.py
@@ -11,12 +11,12 @@ for filename in os.listdir("wordLemPoS"):
                 line_split = line.split()
                 if "_" in line_split[2]:
                     line_split[2] = line_split[2][0:line_split[2].index("_")]   
+                line_split[2] = re.sub(r'[^a-zA-Z0-9]','', line_split[2])
                 if line_split[2][-1].isdigit() and line_split[2][-2].isdigit():
                     line_split[2] = line_split[2][0:-2]
-                line_split[2] = re.sub(r'[^a-zA-Z0-9]','', line_split[2])
                 file.write(line_split[0]+" "+line_split[2]+"\n")
             except:
-                print(line) 
+                print(line)
     f.close()
 file.close()
         
